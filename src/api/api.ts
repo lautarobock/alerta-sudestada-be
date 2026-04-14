673670f3471f7a681cb0208cb30041562aa05536
Lautaro Cozzani <lautaro.cozzani@quorumsoftware.com>
2026-04-14 09:59:52 -0300

    fix BASE URL use ssl

--- src/api/api.ts
+++ src/api/api.ts
@@ -12,14 +12,17 @@ export interface ReadingValue {
 }
 
 export class API {
+
+    private baseUrl = 'https://www.hidro.gov.ar';
+
     constructor() {}
 
     current() {
-        return Axios.get<ReadingData>(`http://www.hidro.gov.ar/api/v1/AlturasHorarias/SFER/${this.now()}`).then(res => res.data);
+        return Axios.get<ReadingData>(`${this.baseUrl}/api/v1/AlturasHorarias/SFER/${this.now()}`).then(res => res.data);
     }
 
     forecast() {
-        return Axios.get('http://www.hidro.gov.ar/oceanografia/pronostico.asp').then(res => res.data as string);
+        return Axios.get(`${this.baseUrl}/oceanografia/pronostico.asp`).then(res => res.data as string);
     }
 
     private now() {
